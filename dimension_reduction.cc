ffac5cad1428f17f8ee08787ffcdef6fb23dc620
sainath <sainath@localhost.localdomain>
2010-04-21 01:10:15 -0400

    FJLT Commit

--- dimension_reduction.cc
+++ dimension_reduction.cc
@@ -1,5 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+/**********************************************************
+	Counts numer of set bits
+/**********************************************************/
+int count_bits(unsigned number){
+	unsigned int c; // c accumulates the total bits set in v
+	while(number){
+	  c++;
+	  number &= number - 1; // clear the least significant bit set
+	}
+}
+
 
 /**********************************************************
 	
@@ -12,12 +26,14 @@
 
 /**********************************************************/
 void randu(float *data, int m, int n){
+	
 	int i,j;
 	for(i=0; i<m; i++){
 		for(j=0; j<n; j++){
 			data[i*n+j] = (float) rand() / RAND_MAX;
 		}
 	}
+
 }
 
 /**********************************************************
@@ -31,11 +47,15 @@ void randu(float *data, int m, int n){
 			an uniform distribution, then two random normally
 			distibuted values can be as follows
 
-			sqrt(2*pi*R1) cos(2*pi*R2)
-			sqrt(2*pi*R1) sin(2*pi*R2)
+			sqrt(-2*log(R1)) cos(2*pi*R2)
+			sqrt(-2*log(R1)) sin(2*pi*R2)
+
+    Formula 30.3 of Statistical Distributions (3rd ed)
+	Merran Evans, Nicholas Hastings, and Brian Peacock
 
 /**********************************************************/
 void randn(float *data, int m, int n){
+
 	float *udata = (float*) malloc( (m*n+1)*sizeof(float));
 	randu(udata, m*n+1, 1);
 
@@ -63,7 +83,143 @@ void randn(float *data, int m, int n){
 	}
 
 	free(udata);
+
 }
+
+/**********************************************************
+	
+	Generate P matrix
+ 	INPUT: Pointer to hold the data
+		   Size of Distribution (k,d)
+		   Epislon e, Embedding type p
+		   Total points n
+	OUTPUT: Resultant P matrix
+
+	Algorithm: P_ij = N(0,1/q) with probability q
+					= 0		   with probability 1-q
+
+				q = min( ( e^p-2 * (log n)^p /d ), 1 )
+				p belongs to {1,2}
+
+/**********************************************************/
+void generatep(float *data, int n, int k, int d, float e, int p){
+
+	float q = exp(p-2) * pow(log(n), p) / d;
+	q = q < 1 ? q : 1;	
+
+	float *rdata = (float*) malloc(k*d*sizeof(float));
+	randu(rdata, k, d);
+
+	randn(data, k, d);
+
+	int i,j;
+	for(i=0; i<k; i++){
+		for(j=0; j<d; j++){
+			data[i*d+j] *= (rdata[i*d+j]<q);
+		}
+	}
+
+}
+
+
+/**********************************************************
+	
+	Generate H matrix
+ 	INPUT: Pointer to hold the data
+		   Size of Distribution (d,d)
+	OUTPUT: Resultant H matrix
+
+	Algorithm: P_ij = d^-0.5 8 (-1)^<i-1,j-1>
+			   <i,j> is the dot product of i,j in binary
+
+/**********************************************************/
+void generateh(float *data, int d){
+
+	float consmul = 1/sqrt(d);
+
+	int i,j;
+	for(i=0; i<d; i++){
+		for(j=0; j<d; j++){
+			int currpow = count_bits( i & j );
+			data[i*d+j] = consmul * ( (currpow & (currpow-1)) == 0 ? 1 : -1 );
+		}
+	}
+
+}
+
+/**********************************************************
+	
+	Generate D matrix
+ 	INPUT: Pointer to hold the data
+		   Size of Distribution (d,d)
+	OUTPUT: Resultant D matrix
+
+	Algorithm: D_ii = {-1,1} with probability 0.5
+			   Stored as a vector with length d to avoid
+			   extra space
+
+/**********************************************************/
+void generated(float *data, int d){
+
+	int i;
+	for(i=0; i<d; i++){
+		data[i] = rand()/RAND_MAX < 0.5 ? 1: -1;
+	}
+
+}
+
+/**********************************************************
+	
+	Generate FJLT Transform matrix
+ 	INPUT: Pointer to hold the data
+		   Size of Distribution (k,d)
+	OUTPUT: Resultant Transform matrix
+
+	Algorithm: FJLT = PHD
+
+/**********************************************************/
+void FJLT(float *data, int n, int k, int d){
+
+	float *P = (float*) malloc( k * d * sizeof(float) );
+	float *H = (float*) malloc( d * d * sizeof(float) );
+	float *D = (float*) malloc( d * sizeof(float) );
+
+	// k = e^-2 * log(n)
+	float e = sqrt(log(n)/k);
+
+	generatep(P, n, k, d, e, 2);
+	generateh(H, d);
+	generated(D, d);
+
+	//FJLT = PHD
+	float *temP = (float*) malloc( k * d * sizeof(float) );
+	memset(temP, 0 , k * d * sizeof(float) ); 
+
+	//Need to multiply the matrices here
+	int a,b,c;	
+	for(a=0; a<k; a++){
+		for(b=0; b<d; b++){
+			register float temp = 0.;
+			for(c=0; c<d; c++){
+				temp += P[a*d+c]*H[c*d+b];
+			}
+			temP[a*d+b] = temp;
+		}
+	}
+
+	//Final matrix multiplication
+	for(a=0; a<k; a++){
+		for(b=0; b<d; b++){
+			register float temp = 0.;
+			for(c=0; c<d; c++){
+				temp += temP[a*d+c]*(c==b?H[c]:0);
+			}
+			data[a*d+b] = temp;
+		}
+	}
+
+}
+
 
 int main(int argc, char *argv[]){
 
